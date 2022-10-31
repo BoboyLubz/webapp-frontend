f4a99988571814d91ba2f3da6cc7041c3fc1e064
BoboyLubz <janrelle.lubiano@carsu.edu.ph>
2022-10-31 10:34:22 +0800

    modified login page, homepage and added the new page. password page and register page

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('homepage'); //ako gi change ang welcome into homepage
 });
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/password', function () {
+    return view('password');
+});
+
